0003f86b60fd279f02ee5d05b968293fda9bde73
Daniel Loiola <danf10@me.com>
2020-03-21 16:15:24 -0300

    testing lang pack

--- client/src/elm/MassiveDecks/Strings/Languages/En.elm
+++ client/src/elm/MassiveDecks/Strings/Languages/En.elm
@@ -1069,7 +1069,7 @@ translate mdString =
             [ Text "English (British)" ]
 
         Português ->
-            [ Text "Português (brasileiro)" ]
+            [ Text "Português brasileiro" ]
 
 
 an : Maybe Int -> String
